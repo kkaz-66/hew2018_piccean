dea99ad6a360b1f024f23eb6100a53388e9a6c9b
awesome036 <hillzen.asia@gmail.com>
2018-11-08 15:08:04 +0900

    awesome ganbatt

--- PicceanDocker/html/index.php
+++ PicceanDocker/html/index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<<<<<<< Updated upstream
 <html>
   <head>
     <meta charset="utf-8">
@@ -15,3 +16,42 @@
       ?>
   </body>
 </html>
+=======
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Piccean</title>
+	<link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+	<div id="topWrapper" class="slideShow">
+		<header>
+			<div class="logoImage"><img src="#" alt="Picceanのロゴ画像"></div>
+			<div class="searchBox">
+				<form action="#検索" method="get">
+					<label for="search">
+						<input type="text" id="search" maxlength="255" placeholder="検索キーワードを入力してください">
+					</label>
+					<input type="submit" id="searchButton" value="検索">
+				</form>
+			</div>
+			<div id="headNavi">
+				<button id="loginButton" onclick="location.href='#'">ログイン</button>
+				<button id="registButton" onclick="location.href='#'">新規登録</button>
+				<button id="cartButton" onclick="location.href='#'">カート</button>
+			</div>
+		</header>
+
+		<footer>
+			<div id="footerNavi">
+				<button class="footerNaviItem" onclick="location.href='#'">お問い合わせ</button>
+				<button class="footerNaviItem" onclick="location.href='#'">利用規約</button>
+				<button class="footerNaviItem" onclick="location.href='#'">プライバシーポリシー</button>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
+>>>>>>> Stashed changes
