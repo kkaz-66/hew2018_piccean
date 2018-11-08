444bdf72a048f5365e8fde3af12e298a1bb882a5
C6STR <c00kie_1dsprst@yahoo.co.jp>
2018-11-08 14:21:03 +0900

    dockercompose.ymlの画像フォルダマウント追記

--- PicceanDocker/html/index.php
+++ PicceanDocker/html/index.php
@@ -3,9 +3,11 @@
   <head>
     <meta charset="utf-8">
     <title>index.html</title>
+    <style type="css"></style>
   </head>
   <body>
   <h1>Piccean<h1>
+    <img src="./images/test.png">
     <?php
     include("./model/db_model.php");
     $test = test();
