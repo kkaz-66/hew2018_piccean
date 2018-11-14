56b5069dd3515265f584cc0d3d23b147435707ef
awesome036 <hillzen.asia@gmail.com>
2018-11-14 16:37:50 +0900

    トップページの枠組みのみ作成

--- PicceanDocker/html/index.php
+++ PicceanDocker/html/index.php
@@ -1,22 +1,4 @@
 <!DOCTYPE html>
-<<<<<<< Updated upstream
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>index.html</title>
-    <style type="css"></style>
-  </head>
-  <body>
-  <h1>Piccean<h1>
-    <img src="./images/test.png">
-    <?php
-    include("./model/db_model.php");
-    $test = test();
-    echo $test["name"];
-      ?>
-  </body>
-</html>
-=======
 <html lang="ja">
 <head>
 	<meta charset="UTF-8">
@@ -26,32 +8,16 @@
 	<link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-	<div id="topWrapper" class="slideShow">
-		<header>
-			<div class="logoImage"><img src="#" alt="Picceanのロゴ画像"></div>
-			<div class="searchBox">
-				<form action="#検索" method="get">
-					<label for="search">
-						<input type="text" id="search" maxlength="255" placeholder="検索キーワードを入力してください">
-					</label>
-					<input type="submit" id="searchButton" value="検索">
-				</form>
-			</div>
-			<div id="headNavi">
-				<button id="loginButton" onclick="location.href='#'">ログイン</button>
-				<button id="registButton" onclick="location.href='#'">新規登録</button>
-				<button id="cartButton" onclick="location.href='#'">カート</button>
-			</div>
-		</header>
+<!-- readHeader -->
+<?php require_once($_SERVER['DOCUMENT_ROOT']."/view/header.php"); ?>
 
-		<footer>
-			<div id="footerNavi">
-				<button class="footerNaviItem" onclick="location.href='#'">お問い合わせ</button>
-				<button class="footerNaviItem" onclick="location.href='#'">利用規約</button>
-				<button class="footerNaviItem" onclick="location.href='#'">プライバシーポリシー</button>
-			</div>
-		</footer>
+	<div class="content">
+	<!-- ここにコンテンツを記述 -->
+		
+	<!-- コンテンツここまで -->
 	</div>
+
+<!-- readFooter -->
+<?php require_once($_SERVER['DOCUMENT_ROOT']."/view/footer.php"); ?>
 </body>
 </html>
->>>>>>> Stashed changes
