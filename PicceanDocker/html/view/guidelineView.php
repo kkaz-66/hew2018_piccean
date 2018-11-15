48b28eb32fce97d8df403f00dc03a46e66577c8e
C6STR <c00kie_1dsprst@yahoo.co.jp>
2018-11-15 14:26:38 +0900

    guidelineView追加、作成中

---
+++ PicceanDocker/html/view/guidelineView.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Piccean</title>
+	<link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<!-- readHeader -->
+<?php require_once($_SERVER['DOCUMENT_ROOT']."/view/header.php"); ?>
+
+	<div class="content">
+	<!-- ここにコンテンツを記述 -->
+	<h1>利用規約</h1>
+	<h2>***について</h2>
+	<h2>***</h2>
+	<!-- コンテンツここまで -->
+	</div>
+
+<!-- readFooter -->
+<?php require_once($_SERVER['DOCUMENT_ROOT']."/view/footer.php"); ?>
+</body>
+</html>
