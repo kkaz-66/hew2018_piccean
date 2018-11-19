d2beb1d2c3973f28bb64fecff9d7779946b4e844
kazms <kaz0953@icloud.com>
2018-11-20 02:08:04 +0900

    [FIX]privacy_policyView.php&css

--- PicceanDocker/html/view/privacy_policyView.php
+++ PicceanDocker/html/view/privacy_policyView.php
@@ -6,6 +6,8 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Piccean</title>
 	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/reset.css">
+	<link rel="stylesheet" href="../css/privacy_policyView.css">
 </head>
 <body>
 <!-- readHeader -->
