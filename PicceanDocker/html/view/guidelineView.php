a7283af00185b06246c22335871b59ddfddb2f84
C6STR <c00kie_1dsprst@yahoo.co.jp>
2018-11-21 14:30:07 +0900

    CREATE guidelineView.phpと.css作成。

--- PicceanDocker/html/view/guidelineView.php
+++ PicceanDocker/html/view/guidelineView.php
@@ -6,22 +6,64 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Piccean</title>
 	<link rel="stylesheet" href="../css/style.css">
-	<link rel="stylesheet" href="../css/guidelineView.css">
+	<link rel="stylesheet" href="../css/reset.css">
+	<link rel="stylesheet" href="../css/privacy_policyView.css">
 </head>
 <body>
 <!-- readHeader -->
-<?php require_once($_SERVER['DOCUMENT_ROOT']."/view/header.php"); ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT']."/view/headerView.php"); ?>
 
 	<div class="content">
 	<!-- ここにコンテンツを記述 -->
-		<div class="guidelinebox">
-		<h1>利用規約</h1>
-		<hr>
-	<!-- コンテンツここまで -->
+		<div class="main">
+			<div class="main2">
+			<h1>利用規約</h1>
+				<h2>第一条</h2>
+					<p>
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+					</p>
+				<h2>第二条</h2>
+					<p>
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+					</p>
+				<h2>第三条</h2>
+					<p>
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+					</p>
+				<h2>第四条</h2>
+					<p>
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+						hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+					</p>
+				<h2>改訂</h2>
+					<p>
+						当社は、本「個人情報の取り扱いについて」の全部または一部を改定することがあります。
+					</p>
+			</div>
 		</div>
+	<!-- コンテンツここまで -->
 	</div>
 
 <!-- readFooter -->
-<?php require_once($_SERVER['DOCUMENT_ROOT']."/view/footer.php"); ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT']."/view/footerView.php"); ?>
 </body>
 </html>
