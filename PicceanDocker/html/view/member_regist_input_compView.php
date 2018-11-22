070e88e802204f300f3522a96038ab9b175b5420
kazms <kaz0953@icloud.com>
2018-11-22 11:40:01 +0900

    [FIX]member_regist_input_completeView.php

--- PicceanDocker/html/view/member_regist_input_compView.php
+++ PicceanDocker/html/view/member_regist_input_compView.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<!--5秒後自動遷移-->
 	<meta http-equiv="refresh"content="5; url=../../index.php">
 	<title>Piccean</title>
 	<link rel="stylesheet" href="../css/style.css">
