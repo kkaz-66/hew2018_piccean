472b878a727110ac4264079a2198cf1c7ac19508
C6STR <c00kie_1dsprst@yahoo.co.jp>
2019-02-05 10:22:15 +0900

    ADD image_addView.php

---
+++ PicceanDocker/html/view/image_addView.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Piccean</title>
+	<link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<!-- readHeader -->
+<?php require_once($_SERVER['DOCUMENT_ROOT']."/view/headerView.php"); ?>
+
+	<div class="content">
+	<!-- ここにコンテンツを記述 -->
+		
+	<!-- コンテンツここまで -->
+	</div>
+
+<!-- readFooter -->
+<?php require_once($_SERVER['DOCUMENT_ROOT']."/view/footerView.php"); ?>
+</body>
+</html>
