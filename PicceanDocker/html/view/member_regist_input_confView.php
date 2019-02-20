24aa67048204cbbd0f2fcae08c6784004bc8239a
awesome036 <hillzen.asia@gmail.com>
2019-02-20 16:44:50 +0900

    [ADD]member_regist_input_conf design css

--- PicceanDocker/html/view/member_regist_input_confView.php
+++ PicceanDocker/html/view/member_regist_input_confView.php
@@ -6,6 +6,7 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>新規登録内容確認ページ</title>
 	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/member_regist_input_conf.css">
 </head>
 <body>
 <!-- readHeader -->
@@ -15,30 +16,32 @@
 	<!-- ここにコンテンツを記述 -->
 
 	<h1>新規登録内容確認</h1>
-	<form action="../controller/member_regist_input_compController.php" method="post">
-		<label for="user_name">氏名</label>
-		<input type="hidden" name="user_name" value="<?= $_POST["user_name"] ?>">
-		<?= $_POST["user_name"]; ?>
-		<br>
-		<label for="user_nickname">ニックネーム</label>
-		<input type="hidden" name="user_nickname" value="<?= $_POST["user_nickname"] ?>">
-		<?= $_POST["user_nickname"]; ?>
-		<br>
-		<label for="user_id">ユーザID</label>
-		<input type="hidden" name="user_id" value="<?= $_POST["user_id"] ?>">
-		<?= $_POST["user_id"]; ?>
-		<br>
-		<label for="user_password">パスワード</label>
-		<input type="hidden" name="user_password" value="<?= $_POST["user_password"] ?>">
-		********
-		<br>
-		<label for="user_email">メールアドレス</label>
-		<input type="hidden" name="user_email" value="<?= $_POST["user_email"] ?>">
-		<?= $_POST["user_email"]; ?>
-		<br>
-		<button type="submit" formaction="../controller/member_regist_inputController.php">戻る</button>
-		<input type="submit" name="submit" value="登録">
-	</form>
+	<div id="inputForm">
+		<form action="../controller/member_regist_input_compController.php" method="post">
+			<label for="user_name">氏名</label>
+			<input type="hidden" name="user_name" value="<?= $_POST["user_name"] ?>">
+			<?= $_POST["user_name"]; ?>
+			<br>
+			<label for="user_nickname">ニックネーム</label>
+			<input type="hidden" name="user_nickname" value="<?= $_POST["user_nickname"] ?>">
+			<?= $_POST["user_nickname"]; ?>
+			<br>
+			<label for="user_id">ユーザID</label>
+			<input type="hidden" name="user_id" value="<?= $_POST["user_id"] ?>">
+			<?= $_POST["user_id"]; ?>
+			<br>
+			<label for="user_password">パスワード</label>
+			<input type="hidden" name="user_password" value="<?= $_POST["user_password"] ?>">
+			********
+			<br>
+			<label for="user_email">メールアドレス</label>
+			<input type="hidden" name="user_email" value="<?= $_POST["user_email"] ?>">
+			<?= $_POST["user_email"]; ?>
+			<br>
+			<button type="submit" formaction="../controller/member_regist_inputController.php">戻る</button>
+			<input type="submit" name="submit" value="登録">
+		</form>
+	</div>
 
 	<!-- コンテンツここまで -->
 	</div>
