5a9f71b5e36630cf7ddef32698b840e8a96edbb0
awesome036 <hillzen.asia@gmail.com>
2019-02-20 17:03:05 +0900

    [FIX]form design

--- PicceanDocker/html/view/member_regist_inputView.php
+++ PicceanDocker/html/view/member_regist_inputView.php
@@ -19,26 +19,26 @@
 	<div id="inputForm">
 		<form action="../controller/member_regist_input_confController.php" method="post">
 			<label for="user_name">氏名</label>
+			<span><?= $_POST["err_name"]; ?></span>
 			<input id="user_name" type="text" name="user_name" value="<?= $_POST["user_name"]; ?>" >
-			<?= $_POST["err_name"]; ?>
 			<label for="user_nickname">ニックネーム</label>
+			<span><?= $_POST["err_nickname"]; ?></span>
 			<input id="user_nickname" type="text" name="user_nickname" value="<?= $_POST["user_nickname"]; ?>" >
-			<?= $_POST["err_nickname"]; ?>
 			<label for="user_id">ユーザID</label>
+			<span><?= $_POST["err_id"]; ?></span>
 			<input id="user_id" type="text" name="user_id" value="<?= $_POST["user_id"]; ?>" >
-			<?= $_POST["err_id"]; ?>
 			<label for="user_password">パスワード</label>
+			<span><?= $_POST["err_password"]; ?></span>
 			<input id="user_password" type="password" name="user_password" >
-			<?= $_POST["err_password"]; ?>
 			<label for="user_email">メールアドレス</label>
+			<span><?= $_POST["err_email"]; ?></span>
 			<input id="user_email" type="email" name="user_email" value="<?= $_POST["user_email"]; ?>" >
-			<?= $_POST["err_email"]; ?>
 			<div class="checkbox">
 				<input type="checkbox" name="checkAgreement" value="1" required>
 				<a href="../view/privacy_policyView.php" target="_brank">利用規約</a>に同意する
 			</div>
 			<input type="submit" name="submit" value="次へ">
-			<?= $_POST["err_msg"]; ?>
+			<span><?= $_POST["err_msg"]; ?></span>
 		</form>
 	</div>
 
