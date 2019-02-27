3cb85426633a7e18394a531845652264ffab8bce
awesome036 <hillzen.asia@gmail.com>
2019-02-27 14:12:59 +0900

    [FIX]fix link

--- PicceanDocker/html/view/headerView.php
+++ PicceanDocker/html/view/headerView.php
@@ -24,7 +24,7 @@
 				<dt>ユーザのアカウント画像</dt>
 				<dd class="menuItem"><a href="../controller/member_info_edit.php">会員情報ページ</a></dd>
 				<dd class="menuItem"><a href="../controller/image_add.php">画像追加ページ</a></dd>
-				<dd class="menuItem"><a href="../controller/member_image_list.php">アップロード画像一覧ページ</a></dd>
+				<dd class="menuItem"><a href="../controller/member_image_listController.php">アップロード画像一覧ページ</a></dd>
 				<dd class="menuItem"><a href="../controller/payment_image_listController.php">購入画像一覧ページ</a></dd>
 			</dl>
 		<?php endif; ?>
